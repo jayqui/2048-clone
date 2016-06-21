8132efa1849b248885b58e90dea0d4dc615b4359
Jay Quigley <jay.quigley@avant.com>
2016-06-21 07:53:10 -0500

    get simple jQuery AJAX working for swipe up

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,6 +1,19 @@
-require 'sinatra'
-
 get '/' do
-  @game = Game.new
-  erb :'index'
+  session[:game] = Game.new
+  erb :'index', locals: {game: session[:game]}
+end
+
+post '/swipe' do
+  if request.xhr?
+    case request.params['move']
+    when 'left'  then session[:game].left
+    when 'right' then session[:game].right
+    when 'up'    then session[:game].up
+    when 'down'  then session[:game].down
+    else
+      status 400
+    end
+
+    erb :'_board', layout: false, locals: {game: session[:game]}
+  end
 end
--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,14 @@
-function keyLeft() {
+$(document).ready(function() {
 
-}
+  $('#posts').on('submit', function(event) {
+    event.preventDefault();
+    var request = $.ajax({
+      method: 'post',
+      url: '/swipe',
+      data: {move: 'up'},
+    });
+    request.done(function(response) {
+      $('#board-frame').replaceWith(response);
+    });
+  });
+});
