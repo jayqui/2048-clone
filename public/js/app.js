27111111539a2ea7cc1fdf33891491dce16b9b26
Jay Quigley <jay.quigley@avant.com>
2016-06-21 09:06:58 -0500

    simplify JS event handling to use arrow key presses

--- public/js/app.js
+++ public/js/app.js
@@ -1,14 +1,22 @@
-$(document).ready(function() {
+function move(direction) {
+  $.ajax({
+          method: 'post',
+          url: '/swipe',
+          data: {move: direction},
+        })
+  .done((response) => {
+    $('#board-frame').replaceWith(response);
+  });
+}
 
-  $('#posts').on('submit', function(event) {
-    event.preventDefault();
-    var request = $.ajax({
-      method: 'post',
-      url: '/swipe',
-      data: {move: 'up'},
-    });
-    request.done(function(response) {
-      $('#board-frame').replaceWith(response);
-    });
+$(document).ready(() => {
+  $(document).keydown((e) => {
+    switch(e.which) {
+      case 37: e.preventDefault(); move('left'); break;
+      case 38: e.preventDefault(); move('up'); break;
+      case 39: e.preventDefault(); move('right'); break;
+      case 40: e.preventDefault(); move('down'); break;
+      default: return;
+    }
   });
 });
